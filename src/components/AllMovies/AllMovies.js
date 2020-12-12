1a33d19eb7a3bba047b757484502f2b5a25feeb0
afiffudin-au <afiffudin077@gmail.com>
2020-12-12 13:43:27 +0700

    improve all movies

--- src/components/AllMovies/AllMovies.js
+++ src/components/AllMovies/AllMovies.js
@@ -2,10 +2,17 @@ import React, { useEffect, useRef } from 'react'
 import { useGetAllMovie } from '../../useAllMovie/useGetAllMovie'
 import Card from '../Card/Card'
 import { StyledLinearProgress } from '../LoadingProgress/LoadingProgress'
+import Pagination from '@material-ui/lab/Pagination';
 import './AllMovies.scss'
 function AllMovies() {
   const {getAllMovie,loading,dataMovies} = useGetAllMovie()
   const refGetAllMovie = useRef(getAllMovie)
+  const [pages, setPages] = React.useState(1);
+  const {total_pages} = dataMovies
+  const handleChange = (event, value) => {
+    setPages(value)
+    getAllMovie(value)
+  };
   useEffect(()=>{
     refGetAllMovie.current()
   },[])
@@ -20,12 +27,37 @@ function AllMovies() {
       <div className="allMoviesGrid">
         {
           dataMovies?.results?.map((item,index)=>(
-            <Card styleProps={{display : 'block',width : '100%'}} id={item.id} releaseDate={item.release_date || item.first_air_date} originalTitle={item.original_title || item.original_name} posterPath={`${process.env.REACT_APP_POSTER_URL}${item.poster_path}`} voteAverage={item.vote_average || 0} key={item.id} />
+            <MemoizedChildComponent
+            styleProps={{display : 'block',width : '100%'}} 
+            id={item.id}
+            releaseDate={item.release_date || item.first_air_date} 
+            originalTitle={item.original_title || item.original_name} 
+            posterPath={`${process.env.REACT_APP_POSTER_URL}${item.poster_path}`} 
+            voteAverage={item.vote_average || 0} key={item.id}
+            />
           ))
         }
+      </div>
+      <div className="allMoviespagenation">
+        <Pagination count={total_pages} page={pages} onChange={handleChange} color="primary"/>
       </div>
     </div>
   )
 }
-
+function ChildComponent({styleProps,id,releaseDate,originalTitle,posterPath,voteAverage}){
+  return(
+    <Card 
+    styleProps={styleProps} 
+     id={id} 
+     releaseDate={releaseDate} 
+     originalTitle={originalTitle}
+     posterPath={posterPath} 
+     voteAverage={voteAverage} />
+  )
+}
+function compare(prevProps , nextProps){
+  return JSON.stringify(prevProps) === JSON.stringify(nextProps)
+}
+//! react.memo -> kalau true component tidak di-render , kalau false di re-render
+const MemoizedChildComponent = React.memo(ChildComponent,compare)
 export default AllMovies
