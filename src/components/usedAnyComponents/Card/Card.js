d9f0312106efe212aad96e941271ccd9c8fdd810
afiffudin-au <afiffudin077@gmail.com>
2021-04-27 22:42:12 +0700

    add placeholder in card component

--- src/components/usedAnyComponents/Card/Card.js
+++ src/components/usedAnyComponents/Card/Card.js
@@ -1,36 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LazyLoad from 'react-lazyload'
-import StarRateIcon from '@material-ui/icons/StarRate';
-import {amber} from '@material-ui/core/colors';
+import StarRateIcon from '@material-ui/icons/StarRate'
+import { amber } from '@material-ui/core/colors'
 import './Card.scss'
-import { useHistory } from 'react-router-dom';
-import { useGetDetail } from '../../../useDetail/useGetDetail';
-import { LightTooltip } from '../../extraComponents/LightTooltip/LightTooltip';
-function Card({url,mediaType,styleProps,id,releaseDate,originalTitle,posterPath,voteAverage}) {
-  const {getDetail} = useGetDetail()
+import { useHistory } from 'react-router-dom'
+import { useGetDetail } from '../../../useDetail/useGetDetail'
+import { LightTooltip } from '../../extraComponents/LightTooltip/LightTooltip'
+function Card({
+  url,
+  mediaType,
+  styleProps,
+  id,
+  releaseDate,
+  originalTitle,
+  posterPath,
+  voteAverage,
+}) {
+  const [display, setDipslay] = useState('none')
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const { getDetail } = useGetDetail()
   const history = useHistory()
-  const handleDetail = ()=>{  
-    getDetail(id,url,mediaType)
+  const handleDetail = () => {
+    getDetail(id, url, mediaType)
     history.push('/detail')
   }
+  const handleImageLoad = () => {
+    setImageLoaded(true)
+    setDipslay('block')
+  }
   return (
-    <div className="Card" style={styleProps}>
-      <div onClick={handleDetail} className="Card-content" >
-        <LazyLoad 
-          width={150}
-          height={200}
-          once={false}
-          >
-          <LightTooltip enterDelay={10} title={originalTitle || ''} placement="top" arrow>
-            <img src={posterPath} alt={''}/>
+    <div className='Card' style={styleProps}>
+      <div onClick={handleDetail} className='Card-content'>
+        {!imageLoaded && <img src='/220x330.png' alt='' />}
+        <LazyLoad width={150} height={200} once={false}>
+          <LightTooltip
+            enterDelay={10}
+            title={originalTitle || ''}
+            placement='top'
+            arrow>
+            <img
+              onLoad={handleImageLoad}
+              style={{ display: display }}
+              src={posterPath}
+              alt={posterPath}
+            />
           </LightTooltip>
         </LazyLoad>
-        <div className="Card_desc">
+        <div className='Card_desc'>
           <h1>{originalTitle}</h1>
           <p>{releaseDate}</p>
         </div>
-        <div className="Card-star-rate bg-blue-600 rounded-full p-1 flex items-center">
-          <StarRateIcon style={{color : amber[600]}} />
+        <div className='Card-star-rate'>
+          <StarRateIcon style={{ color: amber[600] }} />
           <span>{voteAverage}</span>
         </div>
       </div>
